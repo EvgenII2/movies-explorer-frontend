370a0366723df04ece91d1f0b606b6d67e14190d
EvgenII L <genja1988@inbox.ru>
2021-10-26 15:29:57 +0300

    feat: added routs to app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Switch } from "react-router-dom";
 import AboutMe from '../AboutMe/AboutMe';
 import AboutProject from '../AboutProject/AboutProject';
 import Header from '../Header/Header';
@@ -6,18 +7,41 @@ import NavTab from '../NavTab/NavTab';
 import Promo from '../Promo/Promo';
 import Techs from '../Techs/Techs';
 import Footer from '../Footer/Footer';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Promo />
-      <NavTab />
-      <AboutProject />
-      <Techs />
-      <AboutMe />
-      <Footer />
+      <Switch>
+        <Route path="/movies">
+          <Header isAuthorized />
+          movies
+        </Route>
+        <Route path="/saved-movies">
+          <Header isAuthorized />
+          saved-movies
+        </Route>
+        <Route path="/profile">
+          <Header isAuthorized />
+          profile
+        </Route>
+        <Route path="/sign-in">
+          sign-in
+        </Route>
+        <Route path="/sign-up">
+          sign-up
+        </Route>
+        <Route path="/">
+          <Header isAuthorized={false} />
+          <Promo />
+          <NavTab />
+          <AboutProject />
+          <Techs />
+          <AboutMe />
+          <Footer />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,35 @@
 import './Header.css';
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import logo from '../../images/header-logo.svg';
 
 
-function Header() {
+function Header({ isAuthorized }) {
     return (
         <div className="header">
             <img src={logo} alt="логотип в шапке" className="header__logo" />
             <nav>
-                <Link className="header__link header__link_color_white" to="/sign-up">
-                    Регистрация
-                </Link>
-                <Link className="header__link header__link_background-color_green" to="/sign-in">
-                    Войти
-                </Link></nav>
-        </div>
+                {!isAuthorized ? (
+                    <>
+                        <Link className="header__link header__link_color_white" to="/sign-up">
+                            Регистрация
+                        </Link>
+                        <Link className="header__link header__link_background-color_green" to="/sign-in">
+                            Войти
+                        </Link>
+                    </>
+                ) : (
+                    <p>
+                        is not Authorized
+                    </p>
+                )}
+            </nav>
+        </div >
     )
+}
+Header.propTypes = {
+    isAuthorized: PropTypes.bool.isRequired
 }
 
 export default Header;
